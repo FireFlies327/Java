7de2874d6cc5fdaddfcf0e0261aeffad48d2451e
FireFlies327 <48069448+FireFlies327@users.noreply.github.com>
2020-05-24 21:54:51 +0300

    IDZ_2_Inheritance

---
+++ Lab2/src/by/gsu/pms/Speed.java
@@ -0,0 +1,10 @@
+package by.gsu.pms;
+
+public class Speed {
+
+    private double max_kmh;
+
+    public Speed(double max_kmh) { this.max_kmh = max_kmh; }
+
+    public double getSpeed() { return max_kmh; }
+}
---
+++ Lab2/src/by/gsu/pms/TaxiPark.java
@@ -0,0 +1,10 @@
+package by.gsu.pms;
+
+public class TaxiPark {
+
+    private double CarCost;
+
+    public TaxiPark(double CarCost) { this.CarCost = CarCost; }
+
+    public double getTaxiPark() { return CarCost; }
+}
